0fd7f591659177c88515077e64c8c96cb1edebb6
Eric Su <esu1@br005.pvt.bridges.psc.edu>
2018-07-12 10:11:10 -0400

    Added more stats, graphs

--- predictJET/predictedTimeError/errorStats.py
+++ predictJET/predictedTimeError/errorStats.py
@@ -1,6 +1,7 @@
 import sys
 import statistics
 from datetime import timedelta
+import matplotlib.pyplot as plt
 
 sys.path.insert(0, '/pylon5/cc5fp8p/esu1/projects/p04/predictJET/torchNN')
 from misc import load
@@ -19,11 +20,26 @@ def extractErrors(times):
     return errors
 
 def printStats(errors):
+    N = len(errors)
+    print("N:", N)
+    nOverPredictions = len(list(filter(lambda x: x > 0, errors)))
+    nUnderPredictions = N - nOverPredictions
+    print("Number of Overpredictions: ", nOverPredictions)
+    print("Overprediction %: {:0.2f}".format(nOverPredictions/N))
+    print("Number of Underpredictions: ", nUnderPredictions)
+    print("Underprediction %: {:0.2f}".format(nUnderPredictions/N))
     print("Mean:", round(statistics.mean(errors), 2), "Hrs")
     print("Median:", round(statistics.median(errors), 2), "Hrs")
     print("Standard Deviation:", round(statistics.stdev(errors), 2), "Hrs")
+
+def graphErrors(errors):
+    plt.hist(errors)
+    plt.xlabel("Error(hrs)")
+    plt.ylabel("Count")
+    plt.show()
 
 if __name__ == "__main__":
     times = load(sys.argv[1])
     errors = extractErrors(times)
     printStats(errors)
+    graphErrors(errors)
